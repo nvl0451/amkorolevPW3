18e3d7b4e2614740df943d9007778c8e2184bc49
korolev solutions <neoevil25@gmail.com>
2022-01-03 05:17:09 +0500

    Initial Commit

---
+++ amkorolevPW3/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  amkorolevPW3
+//
+//  Created by Андрей Королев on 27.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+class StackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+        // Do any additional setup after loading the view.
+    }
+
+
+}
